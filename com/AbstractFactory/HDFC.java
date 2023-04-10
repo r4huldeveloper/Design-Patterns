191f08e9e3c61373919685c56b0952b1b52f7b66
Rahul Sharma <developerrahul671@gmail.com>
2023-04-10 21:50:08 +0530

    Create HDFC.java

---
+++ com/AbstractFactory/HDFC.java
@@ -0,0 +1,13 @@
+package com.AbstractFactory;
+
+public class HDFC implements Bank{
+    private final String BNAME;
+    public HDFC(){
+        BNAME = "HDFC BANK";
+    }
+
+    @Override
+    public String getBankName(){
+        return BNAME;
+    }
+}
