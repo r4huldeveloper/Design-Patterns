a2883f820a0fae9ba2253899c4e0e930551124f8
Rahul Sharma <developerrahul671@gmail.com>
2023-04-10 21:50:51 +0530

    Create ICICI.java

---
+++ com/AbstractFactory/ICICI.java
@@ -0,0 +1,13 @@
+package com.AbstractFactory;
+
+public class ICICI implements Bank{
+    private final String BNAME;
+    public ICICI(){
+        BNAME = "ICICI BANK";
+    }
+    
+    @Override
+    public String getBankName(){
+        return BNAME;
+    }
+}
